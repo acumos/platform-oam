86a80fc493612101af6a24b6af56ec1e6eca931c
AMIT M <am00474504@techmahindra.com>
2019-03-29 19:22:09 +0530

    Platform maintenance support-backend impl
     Issue-ID: ACUMOS-2004,2343,2344
     -List all the repository present into ElasticStack
     -Create the repository present into ElasticStack
     -Delete the repository present into ElasticStack
     -List all the snapshot present into ElasticStack
     -Create the snapshot present into ElasticStack
     -Delete the snapshot present into ElasticStack
     -Managing Schedule Cron Job for creation Log Snapshot.
    
    Change-Id: I62d414078f77b7a4f8bba22e8d709fccb0e47bea
    Signed-off-by: AMIT M <am00474504@techmahindra.com>

---
+++ elk-client/src/main/java/org/acumos/elk/client/ElkLogSnapshotTaskSchedular.java
@@ -0,0 +1,83 @@
+package org.acumos.elk.client;
+
+import java.lang.invoke.MethodHandles;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.acumos.elk.client.service.ISnapshotRepositoryService;
+import org.acumos.elk.client.service.ISnapshotService;
+import org.acumos.elk.client.transport.CreateSnapshot;
+import org.acumos.elk.client.transport.ELkRepositoryMetaData;
+import org.acumos.elk.client.transport.ElasticsearchSnapshotsResponse;
+import org.acumos.elk.client.transport.ElkCreateSnapshotRequest;
+import org.acumos.elk.client.transport.ElkGetRepositoriesResponse;
+import org.acumos.elk.client.transport.ElkGetSnapshotMetaData;
+import org.acumos.elk.client.transport.ElkSnapshotsResponse;
+import org.acumos.elk.client.utils.ELKClientConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Managing Schedule cron job for snapshot creation.
+ *
+ */
+@Component
+public class ElkLogSnapshotTaskSchedular {
+
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	@Autowired
+	ISnapshotService snapshotService;
+
+	@Autowired
+	ISnapshotRepositoryService snapshotGetRepositoryService;
+
+	@Scheduled(cron = "${elk.cronschedule.createsnapshot.time}")
+	public void createLogSnapshot() throws Exception {
+		logger.debug("Inside createLogSnapshot");
+		logger.info("The time is now {}", dateFormat.format(new Date()));
+		try {
+
+			ElkGetRepositoriesResponse elkGetRepositoriesResponse = snapshotGetRepositoryService.getAllElkRepository();
+			List<ELkRepositoryMetaData> repositories = elkGetRepositoriesResponse.getRepositories();
+
+			for (ELkRepositoryMetaData eLkRepositoryMetaData : repositories) {
+				ElkCreateSnapshotRequest createDeleteSnapshotRequest = new ElkCreateSnapshotRequest();
+				List<CreateSnapshot> createSnapshots = new ArrayList<>();
+				CreateSnapshot createSnapshot = new CreateSnapshot();
+				createSnapshot.setCreate(ELKClientConstants.TRUE);
+				createSnapshot.setRepositoryName(eLkRepositoryMetaData.getName());
+				createSnapshots.add(createSnapshot);
+				createDeleteSnapshotRequest.setCreateSnapshots(createSnapshots);
+				createDeleteSnapshotRequest.setNodeTimeout("1");
+				ElkSnapshotsResponse response = snapshotService
+						.createElasticSearchSnapshot(createDeleteSnapshotRequest);
+				List<ElasticsearchSnapshotsResponse> elasticsearchSnapshots = response.getElasticsearchSnapshots();
+
+				for (ElasticsearchSnapshotsResponse elasticsearchSnapshotsResponse : elasticsearchSnapshots) {
+					logger.debug("RepositoryName: {}", elasticsearchSnapshotsResponse.getRepositoryName());
+					List<ElkGetSnapshotMetaData> snapshots = elasticsearchSnapshotsResponse.getSnapshots();
+					for (ElkGetSnapshotMetaData elkGetSnapshotMetaData : snapshots) {
+						logger.debug(
+								"\n########################################## " + "\n SnapShotId:{}  \nStatus:{}"
+										+ "\n########################################## ",
+								elkGetSnapshotMetaData.getSnapShotId(), elkGetSnapshotMetaData.getStatus());
+					}
+				}
+
+			}
+
+		} catch (Exception e) {
+			logger.debug("Exception", e);
+			throw e;
+		}
+		logger.info("The time is now {}", dateFormat.format(new Date()));
+	}
+
+}
